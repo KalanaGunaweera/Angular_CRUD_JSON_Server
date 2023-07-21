5b17be5c68249d738d3cdb69d266c0e899e2b548
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-07-21 20:13:36 +0530

    Create form and link with ts file in signup form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  public signupForm!: FormGroup;
+  constructor(private formBuilder : FormBuilder , private http:HttpClient){}
+
+  ngOnInit(): void {
+   this.signupForm = this.formBuilder.group({
+    fullName:[''],
+    mobile:[''],
+    email:[''],
+    password:['']
+   })
+  }
+  signUp(){
+    
+  }
+
 
 }

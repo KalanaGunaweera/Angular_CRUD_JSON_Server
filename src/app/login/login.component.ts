5a4bc5386a3a9be38e251e00091bd125fe56970a
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-07-21 21:46:51 +0530

    Enter validation for login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup,Validator, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -15,8 +15,8 @@ export class LoginComponent implements OnInit {
   constructor(private formBuilder : FormBuilder,private http : HttpClient, private router : Router){}
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email:[''],
-      password:['']
+      email:['',Validators.required],
+      password:['',Validators.required]
     })
   }
 

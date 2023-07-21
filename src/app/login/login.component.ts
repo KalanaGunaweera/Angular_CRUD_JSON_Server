e6a1516ec8fa8793165cd921c508f8df94313190
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-07-21 21:06:46 +0530

    create loging access and setup http get_request

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,9 +20,23 @@ export class LoginComponent implements OnInit {
     })
   }
 
-  // login(){
-  //   this.http.get<any>("http://localhost:3000/signupUsers")
+  login(){
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user){
+        alert("Login Success!!");
+        this.loginForm.reset();
+        this.router.navigate(['dashboard'])
+      }else{
+        alert("User not found!!");
+      }
+    },err=>{
+      alert("Something went wrong!!")
+    })
 
-  // }
+  }
 
 }

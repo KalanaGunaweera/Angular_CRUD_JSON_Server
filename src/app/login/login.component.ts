e8017ae1857092fba767630e6a8ae775beffdc25
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-07-21 20:53:59 +0530

    Login for connect with html and ts file_FormGroup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
+
+
+  constructor(private formBuilder : FormBuilder,private http : HttpClient, private router : Router){}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email:[''],
+      password:['']
+    })
+  }
+
+  // login(){
+  //   this.http.get<any>("http://localhost:3000/signupUsers")
+
+  // }
 
 }
